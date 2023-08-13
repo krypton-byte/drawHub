504d5b87893840aeceb87d81176b5c02c2c29e19
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-08-14 02:15:25 +0530

    Update pillow version

--- phlogo/__init__.py
+++ phlogo/__init__.py
@@ -18,7 +18,7 @@ def Gabung(fun):
         im,text1=add_corners(arg[0], 17),arg[1]
         op=Image.new("RGB",(40,20),color=(0,0,0))
         draw=ImageDraw.Draw(op)
-        size=font.getsize(text1)
+        size=font.getbbox(text1)
         baru=Image.new("RGB",(im.width+size[0]+210+20+130,600), color=(0,0,0))
         draw=ImageDraw.Draw(baru)
         draw.text((150,250), text1,(255,255,255),font=font)
@@ -26,7 +26,7 @@ def Gabung(fun):
         return baru
     return gabung(fun)
 def generate(text1, text2):
-    panjangText=font.getsize(text2)
+    panjangText=font.getbbox(text2)
     oren=Image.new("RGBA",(panjangText[0]+20,140),color=(240, 152, 0))
     draw=ImageDraw.Draw(oren)
     draw.text((10,int((oren.height-panjangText[1])/2)-10),text2, (0,0,0),font=font)
